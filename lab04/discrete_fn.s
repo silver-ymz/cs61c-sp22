06d7936ff2226c1339172ada6c63b2859cf0a83d
salvor <yinmingzhuo@gmail.com>
2023-03-28 23:11:15 +0800

    lab04 ex1 complete

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -101,7 +101,10 @@ main:
 # a1 is the address of the "output" array (defined above).
 f:
     # YOUR CODE GOES HERE!
-
+    addi t0, a0, 3
+    slli t0, t0, 2
+    add t0, t0, a1
+    lw a0, 0(t0)
     jr ra               # Always remember to jr ra after your function!
 
 # prints out one integer

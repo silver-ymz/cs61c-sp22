4aa51dc580270ec1f029ab4d69614c2ee73daa1e
salvor <yinmingzhuo@gmail.com>
2023-03-28 22:03:58 +0800

    lab03 ex4 complete

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,12 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    addi t0, x0, 1 # t0 is ans
+loop:
+    beq a0, x0, exit
+    mul t0, t0, a0
+    addi a0, a0, -1
+    jal x0, loop
+exit:
+    add a0, x0, t0
+    jr ra

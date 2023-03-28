31a2596331619c4932ba38adf7b4ff7b6afc9637
salvor <yinmingzhuo@gmail.com>
2023-03-28 18:33:54 +0800

    lab01 ex4 complete

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,18 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head, *slow_ptr = head;
+
+    while (1) {
+        if (fast_ptr == NULL || fast_ptr->next == NULL) {
+            return 0;
+        }
+
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+    }
 }
